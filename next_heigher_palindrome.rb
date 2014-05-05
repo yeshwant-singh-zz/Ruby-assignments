7556403df7a15ab3ee3d71b602f5de0a6b9f2842
yeshwant-singh <yeshwant@browserstack.com>
2014-05-05 19:52:26 +0530

    changes to include carry forward

--- next_heigher_palindrome.rb
+++ next_heigher_palindrome.rb
@@ -48,20 +48,57 @@ def find_next_palindrome(test_num)
 	if original_array.size.even?
 		mid =original_array.size/2
 		count = 0 
-		while test_num.join().to_i <= original_num 
-			test_num[mid-1-count] = test_num[mid-1-count] + 1      #incrementing the middle numbers
-			
-			test_num[mid+count] = test_num[mid+count] + 1
-			count = count + 1
+		while test_num.join().to_i <= original_num
+			if test_num[mid-1-count] < 9 
+					#puts mid-2-count
+				test_num[mid-1-count] = test_num[mid-1-count] + 1      #incrementing the middle numbers and going forward
+				test_num[mid+count] = test_num[mid+count] + 1
+				#puts "loop1#{test_num}"
+				
+				count = count + 1 if !test_num[mid+count+1].nil?
+			elsif test_num[mid-1-count] >= 9 && !test_num[mid+count+1].nil?
+					#puts "loop2"
+				test_num[mid-1-count] = 0
+				test_num[mid+count] = 0
+				#print test_num
+			else
+				puts "new array is overflowing"
+				new_array = [1,[0]*(test_num.size-1),1].flatten
+				return new_array
+			end
+
 		end
 	end
 
 	if original_array.size.odd?
 		mid =original_array.size/2
 		count = 0 
+		offset = 0
 
 		while test_num.join().to_i <= original_num 				#incrementing the central number
 			test_num[mid] = test_num[mid] + 1
+			#print test_num
+			if test_num[mid] >9 && test_num[mid-offset] > 9
+				#puts "loop1"
+				test_num[mid] = 0
+				test_num[mid-offset] = 0
+				test_num[mid+offset] = 0
+				offset = offset + 1
+				test_num[mid-offset] = test_num[mid-offset]+1 if test_num[mid-offset] < 9
+				test_num[mid+offset] = test_num[mid+offset]+1 if test_num[mid-offset] < 9
+			elsif test_num[mid] > 9 && test_num[mid-offset] < 9
+				#puts "loop2"
+				test_num[mid] = 0
+				test_num[mid-offset] = test_num[mid-offset]+1 if  test_num[mid-offset] < 9
+				test_num[mid+offset] = test_num[mid+offset]+1 if test_num[mid-offset] < 9
+			elsif test_num[mid+offset+1].nil? && test_num[mid] >= 9 && test_num[mid-offset] >= 9
+				#puts test_num[mid+offset+1]
+				puts "new array is over flowing"
+				new_array = [1,[0]*(test_num.size-1),1].flatten
+				return new_array
+			else
+				next
+			end
 		end
 	end
 	puts ""
@@ -73,13 +110,13 @@ puts "Enter array of intergers seperated by comma like 2,3,4,5"  #comment these 
 number = gets								#comment these 5 lines for disabling user input
 num = number.split(",")					#comment these 5 lines for disabling user input
 num = num.collect {|x| x.to_i}				#comment these 5 lines for disabling user input
-print find_next_palindrome(num)				#comment these 5 lines for disabling user input
+print "#{num} => #{find_next_palindrome(num)}\n"				#comment these 5 lines for disabling user input
 
 #num = number.to_s.split("")
 puts "some more test data------------------"
-print "next heigher palindrome is: #{find_next_palindrome([1,2,3,7,7,])}\n"
-print "next heigher palindrome is: #{find_next_palindrome([1,2])}\n"
-print "next heigher palindrome is: #{find_next_palindrome([1,2,3,7])}\n"
+print "next heigher palindrome : #{find_next_palindrome([1,2,3,7,7,])}\n"
+print "next heigher palindrome : #{find_next_palindrome([1,2])}\n"
+print "next heigher palindrome : #{find_next_palindrome([1,2,3,7])}\n"
 puts find_next_palindrome([1,"2"])
 puts find_next_palindrome(["1avavvvv",2])
 puts find_next_palindrome([13434,2])
